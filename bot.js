1566280c900cc39dbfa3926b2efe8afb4b4bac51
Luciano Mammino <lmammino@oryzone.com>
2015-08-11 00:12:01 +0200

    Added leveldb integration

--- bot.js
+++ bot.js
@@ -2,10 +2,17 @@
 
 // https://github.com/mishk0/slack-bot-api
 
+var path = require('path');
+var fs = require('fs');
 var SlackBot = require('slackbots');
+var levelup = require('level');
 
 // get the token from https://<yourorganization>.slack.com/services/new/bot
 var token = process.env.BOT_API_KEY || require('./token');
+var channel = process.env.BOT_CHANNEL || 'general';
+var dbPath = process.env.BOT_DB_PATH || path.resolve(__dirname, 'db');
+
+var db = null;
 
 // create a bot
 var bot = new SlackBot({
@@ -14,6 +21,17 @@ var bot = new SlackBot({
 });
 
 bot.on('start', function () {
+    if (!fs.existsSync(dbPath)) {
+        fs.mkdirSync(dbPath);
+    }
+
+    db = levelup(dbPath);
+    db.put('info~lastrun', new Date());
+
     // more information about additional params https://api.slack.com/methods/chat.postMessage
-    bot.postMessageToChannel('general', 'Hi guys, roundhouse-kick anyone?', {as_user: true});
+    bot.postMessageToChannel(channel, 'Hi guys, roundhouse-kick anyone?', {as_user: true});
+});
+
+bot.on('message', function (data) {
+    console.log(data);
 });
