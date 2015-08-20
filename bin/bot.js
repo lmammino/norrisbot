5b5b9e3c9e87da8e56542186655e9c09f327c8bc
Luciano Mammino <lmammino@oryzone.com>
2015-08-20 18:18:05 +0200

    reviewed code and first working prototype

--- bin/bot.js
+++ bin/bot.js
@@ -11,7 +11,7 @@ var NorrisBot = require('../lib/norrisbot');
 // get the token from https://<yourorganization>.slack.com/services/new/bot
 var token = process.env.BOT_API_KEY || require('../token');
 var dbPath = process.env.BOT_DB_PATH;
-var name = process.env.BOT_NAME || 'norrisbot';
+var name = process.env.BOT_NAME;
 
 var norrisbot = new NorrisBot({
     token: token,
