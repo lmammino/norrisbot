c0343fd1c308170833d32d8e967f2740b32dba57
Luciano Mammino <luciano@planet9energy.com>
2017-04-21 15:38:05 +0100

    Cleaned up old code and added new db-file and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": "airbnb",
+  "env": {
+    "node": true,
+    "jest": true,
+    "es6": true
+  },
+  "rules": {
+    "import/prefer-default-export": 0,
+    "import/no-named-as-default-member": 0,
+    "max-len": 0,
+    "camelcase": 0
+  }
+};
