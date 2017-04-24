f3860c4d70221b550da7fc19d23f4041a91b6f2a
Luciano Mammino <lucianomammino@gmail.com>
2017-04-24 20:28:43 +0100

    Added help in CLI

--- src/norrisbot-cli.js
+++ src/norrisbot-cli.js
@@ -1,7 +1,30 @@
 #!/usr/bin/env node
 
+/* eslint no-console: off */
+
 import winston from 'winston';
+import chalk from 'chalk';
 import norrisbot from './norrisbot';
+import pkg from '../package.json';
+
+if (['help', '--help', '-h', 'version', '--version', '-v'].includes(process.argv[2])) {
+  console.log(`
+    ${chalk.bgMagenta(`norrisbot v${pkg.version}`)}
+
+    Usage:
+
+    ${chalk.cyan('norrisbot')}
+
+    Configuration through environment variables:
+
+    ${chalk.cyan('NORRISBOT_TOKEN')}         - ${chalk.grey('(Mandatory)')} The Slack Bot User OAuth Access Token for your organisation/team
+    ${chalk.cyan('NORRISBOT_TRIGGERS')}      - ${chalk.grey('(Optional)')} A coma separated list of words that triggers the bot to reply with a joke (${chalk.grey('Default')}: "Chuck Norris,norrisbot")
+    ${chalk.cyan('NORRISBOT_CATEGORIES')}    - ${chalk.grey('(Optional)')} A coma separated list to enable special joke categories like "explicit" and "nerdy" (${chalk.grey('Default')}: "nerdy")
+    ${chalk.cyan('NORRISBOT_NO_PICTURES')}   - ${chalk.grey('(Optional)')} If set to TRUE will disable pictures in jokes (${chalk.grey('Default')}: "FALSE")
+    ${chalk.cyan('NORRISBOT_MESSAGE_COLOR')} - ${chalk.grey('(Optional)')} The hex color used by the bot to mark it's messages (${chalk.grey('Default')}: "#590088")
+  `);
+  process.exit(0);
+}
 
 const logger = new winston.Logger({
   transports: [
