5d4541cdc65851c60a19742c9939561030bc70a9
Luciano Mammino <lucianomammino@gmail.com>
2017-04-25 22:49:29 +0100

    cleanup

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 ./node_modules/.bin/rimraf lib;
-mkdir ./lib;
 ./node_modules/.bin/babel src --out-dir lib;
 chmod +x lib/norrisbot-cli.js;
--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc;
-npm publish;
